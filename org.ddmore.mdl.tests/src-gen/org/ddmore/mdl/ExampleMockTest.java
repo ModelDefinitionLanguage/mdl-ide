a9bd87e1561b139bb47ef8fad048f11f328eb9b6
matsrogalski <mrogalski@mango-solutions.com>
2013-04-04 17:02:53 +0100

    DDMORE-75: Enabled tests in maven build.

---
+++ org.ddmore.mdl.tests/src-gen/org/ddmore/mdl/ExampleMockTest.java
@@ -0,0 +1,16 @@
+package org.ddmore.mdl;
+
+import org.junit.Test;
+/**
+ * That is an example test case with a single example mock test.
+ * @author mrogalski
+ *
+ */
+public class ExampleMockTest {
+
+	@Test
+	public void shouldPrintAHelloWorldMessageToTheConsole() {
+		System.out.println("Hello world, I just have been tested!");
+	}
+
+}
