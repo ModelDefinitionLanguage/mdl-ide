27da159a3105f42553c1d58cf22aa2ee88f7c4d9
Natallia Kokash <natallia.kokash@gmail.com>
2014-12-03 10:12:33 +0100

    Menu items for obsolete conversion to NONMEM are hidden.

--- org.ddmore.mdl/src/org/ddmore/mdl/domain/Variable.java
+++ org.ddmore.mdl/src/org/ddmore/mdl/domain/Variable.java
@@ -1,14 +1,21 @@
 package org.ddmore.mdl.domain;
 
+//import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 import org.ddmore.mdl.types.MdlDataType;
 
 public class Variable {
 	String name;
 	MdlDataType type = MdlDataType.TYPE_UNDEFINED; 
+	HashMap<String, Variable> dependencies = null;
+	List<String> tmpDependencies = null;
 	
-	//public Variable(String name){
-	//	this.name = name;
-	//}	
+	public Variable(String name, List<String> tmpDependencies){
+		this.name = name;
+		this.tmpDependencies = tmpDependencies;
+	}	
 	
 	public Variable(String name, MdlDataType type){
 		this.name = name;
@@ -21,6 +28,40 @@ public class Variable {
 	
 	public MdlDataType getType(){
 		return type;
+	}
+	
+	public boolean addDependency(Variable newDependency){
+		if (!dependencies.containsKey(newDependency.getName())){
+			dependencies.put(newDependency.getName(), newDependency);
+			return true;
+		}
+		return false;
+	}
+
+	public boolean addTmpDependency(String newTmpDependency){
+		if (!tmpDependencies.contains(newTmpDependency)){
+			tmpDependencies.add(newTmpDependency);
+			return true;
+		}
+		return false;
+	}
+	
+	public void clearDependencies(){
+		dependencies.clear();
+		dependencies = null;
+	}
+
+	public void clearTmpDependencies(){
+		tmpDependencies.clear();
+		tmpDependencies = null;
+	}
+
+	public HashMap<String, Variable> getDependencies(){
+		return dependencies;
+	}
+
+	public List<String> getTmpDependencies(){
+		return tmpDependencies;
 	}
 	
 }
