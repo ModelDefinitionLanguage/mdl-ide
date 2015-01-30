7708f1dbb11038e3600fe9d1612ff222ce8032fa
matsrogalski <mrogalski@mango-solutions.com>
2015-01-30 15:23:26 +0000

    DDMORE-1031: Updates to dependencies resolving issue with invoking libpharmml. Added launch configuration that runs JUnit tests within MDL IDE instance. Added unit test invoking libpharmml. Removed old tests launch configuration which now is redundant.

---
+++ org.ddmore.mdl.tests/src/org/ddmore/libpharmml/LibPharmMLTest.java
@@ -0,0 +1,50 @@
+package org.ddmore.libpharmml;
+
+import static org.junit.Assert.fail;
+
+import java.io.InputStream;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.log4j.Logger;
+import org.junit.Test;
+
+import eu.ddmore.libpharmml.ILibPharmML;
+import eu.ddmore.libpharmml.IPharmMLResource;
+import eu.ddmore.libpharmml.IPharmMLValidator;
+import eu.ddmore.libpharmml.IValidationError;
+import eu.ddmore.libpharmml.IValidationReport;
+import eu.ddmore.libpharmml.PharmMlFactory;
+
+/**
+ * Tests integration with libPharmML
+ */
+public class LibPharmMLTest {
+
+    private static final Logger LOG = Logger.getLogger(LibPharmMLTest.class);
+
+    @Test
+    public void shouldValidatePharmMLResource() {
+        ILibPharmML libPharmML = PharmMlFactory.getInstance().createLibPharmML();
+        InputStream in = null;
+        String modelFile = "/pharmml/0.3.1/warfarin_PK_ODE/Warfarin-ODE-latest.xml";
+        try {
+            in = LibPharmMLTest.class.getResourceAsStream(modelFile);
+            IPharmMLResource res = libPharmML.createDomFromResource(in);
+            IPharmMLValidator validator = libPharmML.getValidator();
+            IValidationReport rpt = validator.createValidationReport(res);
+            if (rpt.isValid()) {
+                LOG.info(modelFile + " is valid");
+            } else {
+                for (int i = 1; i <= rpt.numErrors(); i++) {
+                    IValidationError err = rpt.getError(i);
+                    LOG.info("Error " + (i) + ": " + err.getErrorMsg());
+                }
+                fail("The validation failed");
+            }
+        } finally {
+            if (in != null) {
+                IOUtils.closeQuietly(in);
+            }
+        }
+    }
+}
